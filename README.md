a080f41e8791f580d4d6ef359456bcbac247a913
Luning-Yang <56569117+Luning-Yang@users.noreply.github.com>
2024-07-10 16:36:42 -0700

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # ChatGPT-Misinfo-Detection
+## Is Truth Relative for Large Language Models? Investigating the Use of Role-playing Prompts on Misinformation Detection Accuracy of ChatGPT
+
+**Michael Robert Haupt<sup>\*</sup>, MA<sup>1,2</sup>, Luning Yang<sup>\*</sup>, BS<sup>2</sup>, Tina Purnat, MSc<sup>3</sup>, Tim K. Mackey, MAS, PhD<sup>2,4,5</sup>**
+
+<sup>1</sup>Department of Cognitive Science, University of California San Diego, 9500 Gilman Dr, La Jolla, CA, 92093, USA  
+<sup>2</sup>Global Health Policy & Data Institute, San Diego, CA USA  
+<sup>3</sup>TH Chan School of Public Health, Harvard University, Boston, MA, USA  
+<sup>4</sup>S-3 Research, San Diego, CA USA  
+<sup>5</sup>Global Health Program, Department of Anthropology, University of California, San Diego, CA USA
+
+\*both authors have contributed equally to this research
+
+**Address for Correspondence:** Tim K. Mackey
+
+9500 Gilman Dr., Mail Code: 0505  
+La Jolla, CA, 92093, USA  
+
+**Email:** [tmackey@ucsd.edu](mailto:tmackey@ucsd.edu)
+
+## Data Availability
+Our raw dataset is located in the *data* folder of this repository and is named *MTurk_Empathy_Data.xlsx*. The result of our 30-iteration experiment is located in the *result* folder and is named *coded_results.xlsx*. We also provide all the code for our experiments in the *src* folder to facilitate reproduction of our results.
+
+## Result Reproduction
+To reproduce our results, you should have all the required packages installed. We recommend creating a virtual environment before running the following command:
+```
+pip install -r requirements.txt
+```
+
+Once you have installed all the required packages, you can start running the experiment.
+
+The parameters you can pass in are `api_key`, `output_file`, `model`, `temperature`, and `iteration`.
+
+- `api_key`: Your OpenAI key. This *must* be provided for the experiment.
+- `output_file`: The name of the output file. Default: *result/coded_results.xlsx*
+- `model`: The OpenAI model for the experiment. Default: *gpt-3.5-turbo*
+- `temperature`: The degree of variability in the model's responses, ranging from 0 to 2. Note that a temperature of 0 means no variability at all. If you pass in the same prompt with a temperature of 0, you will always get the same response. Default: *0.7*
+- `iteration`: The number of iterations for the same prompt. Note that if the temperature is set to 0, this will always be reset to 1 regardless of the initial setting. Since there is no variability, it is unnecessary to create more than one iteration. Default: *30*
+
+Except for `api_key`, you can leave the parameters unspecified, and they will be set to their default values. Below is a sample code for the experiment:
+```
+python src/generate.py --api_key="Your API Key" --iteration=2 --output_file="result/output_file.xlsx"
+```
+
+If you wish to run with the exact parameter settings as ours (using the default values), run:
+```
+python src/generate.py --api_key="Your API Key"
+```
+*Note*: You won't be able to produce the exact same results even if you use the exact same parameters as ours due to the variability introduced by the temperature parameter.
---
+++ src/generate.py
@@ -0,0 +1,164 @@
+import pandas as pd
+import fire
+import warnings
+from langchain.chat_models import ChatOpenAI 
+from langchain.chains import LLMChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+def main(
+    output_file: str = "result/coded_results.xlsx",
+    model: str = "gpt-3.5-turbo",
+    api_key: str | None = None,
+    temperature: float = 0.7,
+    iteration: int = 30
+):
+    assert api_key is not None, "API key is not provided."
+    
+    if temperature == 0 and iteration > 1:
+        warnings.warn(("When the temperature is set to 0, there is no variability in the output for "
+                        "the same prompt. Therefore, the iteration count is set to 1."))
+        iteration = 1
+        
+    
+    # Load the data
+    df = pd.read_excel('data/MTurk_Empathy_Data.xlsx', header=None, sheet_name='Tweet_Text_Coding')
+
+    # Select the first two rows.
+    texts = df.iloc[0].tolist()[1::5]
+    tags = df.iloc[1].tolist()[1::5]
+    tweets = pd.DataFrame({'text':texts, 'origin_tag':tags})
+
+    # Define the prompt templates
+    template = "You are a helpful assistant."
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+
+    human_template = """
+    Suppose you are a person with {education} degree and live in {place}. \\
+    You are {political_belief}, {religion}, and {personality}.
+
+    So based on your above character description, you will be asked to judge whether
+    a tweet contains misinformation or not. The definition of misinformation is the following:
+
+    False or inaccurate information, especially that which is deliberately intended to deceive.
+
+    Do you think the following tweet contains misinformation?
+
+    Yes: the tweet contains misinformation
+    No: the tweet does NOT contain misinformation
+
+    Be sure to align your reasoning with your identity description above in this form: ‘Choice:__### Reason:__‘\\
+    (make sure to use ### as the delimiter).
+
+    Tweet begins:
+    {tweet}
+    """
+
+
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+    # Initialize the OpenAI model
+    llm = ChatOpenAI(
+        openai_api_key=api_key,
+        model_name=model,
+        temperature=temperature,
+    )
+    chain = LLMChain(llm=llm, prompt=chat_prompt)
+
+    # Generate responses
+    output = []
+    for tweet in tweets['text']:
+        for edu in ['high school', 'undergraduate degree', 'graduate']: 
+            for place in ['rural', 'urban']:
+                for personality in ['narcissistic', 'empathetic']: 
+                    for religion in ['religious', 'atheistic']:
+                        for belief in ['Liberal', 'Conservative']: 
+                            char_dict = {
+                                'education': edu, 
+                                'place': place, 
+                                'political_belief': belief,
+                                'religion': religion, 
+                                'personality': personality, 
+                                'tweet': tweet
+                            }
+                            for i in range(iteration):
+                                char_dict[f'response_{i+1}'] = chain.run(char_dict)
+                                output.append(char_dict)
+
+    df = pd.DataFrame(output)
+
+    #check the presence of the variables
+    undergraduate = ['undergrad', 'undergrads', 'undergraduate', 'undergraduated', 
+                 'undergraduates', 'undergraduating','undergraduation']
+
+    graduate = ['graduand', 'graduands', 'graduate', 'graduated', 'graduates',
+                'graduating', 'graduation']
+
+    rural = ['rural', 'rurality', 'rurally']
+
+    urban = ['urban', 'urbanely', 'urbanite', 'urbanity', 'urbanization', 'urbanize', 'urbanized']
+
+    liberal = ['liberal', 'liberalism', 'liberalist', 'liberalistic', 'liberally', 'liberals']
+
+    conservative = ['conservatism', 'conservative', 'conservatively', 'conservativeness']
+
+    religious = ['religion', 'religious', 'religiously']
+
+    atheistic = ['atheism', 'atheist', 'atheistic', 'atheistically']
+
+    narcissistic = ['narcissism', 'narcissistic', 'narcissistically']
+
+    empathetic = ['empath', 'empathetic', 'empathetically', 'empathise', 'empathised',
+                  'empathising', 'empathize', 'empathized','empathizing']
+
+
+    education = undergraduate + graduate + ['high school']
+    place = rural + urban
+    political = liberal + conservative
+    religion = religious + atheistic
+    personal = narcissistic + empathetic
+
+    def mention_variable(text):
+        text = text.lower()
+        result_dict = {key:0 for key in ['Edu', 'Place', 'Political', 'Relig', 'Personal']}
+
+        for edu in education:
+            if edu in text: 
+                result_dict['Edu'] = 1 
+                break
+        for pl in place: 
+            if pl in text:
+                result_dict['Place'] = 1
+                break
+        for poli in political:
+            if poli in text: 
+                result_dict['Political'] = 1
+                break
+        for relig in religion: 
+            if relig in text:
+                result_dict['Relig'] = 1
+                break
+        for person in personal:
+            if person in text: 
+                result_dict['Personal'] = 1 
+                break
+
+        return result_dict
+
+
+    for i in range(iteration):
+        df[f'Choice_{i+1}'] = df[f'response_{i+1}'].apply(lambda x: 1 if 'Choice: Yes' in x else 0)
+        df[f'variable_presence_{i+1}'] = df[f'response_{i+1}'].apply(mention_variable)
+
+    df.to_excel(output_file, index=None)
+
+if __name__ == "__main__":
+    fire.Fire(main)
+
+
+
+
