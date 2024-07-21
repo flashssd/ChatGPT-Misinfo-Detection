1478fa0d0e697679a504477fd18523f24aa9807d
flashssd <l4yang.ucsd@gmail.com>
2024-07-21 15:09:31 -0700

    analysis update

--- src/analysis.py
+++ src/analysis.py
@@ -12,15 +12,10 @@ def analyze(df: pd.DataFrame, iteration: int, identity: int) -> None:
     # no identity
     if identity == 0:
         output_file = "result/analysis_no_identity.xlsx"
+        # average yes by tweets
         weights = df[["tweet", "TotalChoice"]]
         excel_write(weights, output_file, "weights")
-
-    # poli only
-    elif identity == 1:
-        output_file = "result/analysis_poli_only.xlsx"
-        weights = df.groupby("tweet")["TotalChoice"].agg(["std", "mean"]).reset_index()
-        excel_write(weights, output_file, "weights")
-
+        # average yes by tweet class
         weights_tweet_class = (
             df.groupby("Tweet_classification")["TotalChoice"]
             .agg(["std", "mean"])
@@ -28,6 +23,20 @@ def analyze(df: pd.DataFrame, iteration: int, identity: int) -> None:
         )
         excel_write(weights_tweet_class, output_file, "weights_tweet_class")
 
+    # poli only
+    elif identity == 1:
+        output_file = "result/analysis_poli_only.xlsx"
+        # average yes by tweets
+        weights = df.groupby("tweet")["TotalChoice"].agg(["std", "mean"]).reset_index()
+        excel_write(weights, output_file, "weights")
+        # average yes by tweet class
+        weights_tweet_class = (
+            df.groupby("Tweet_classification")["TotalChoice"]
+            .agg(["std", "mean"])
+            .reset_index()
+        )
+        excel_write(weights_tweet_class, output_file, "weights_tweet_class")
+        # average yes by poli belief
         weights_poli = (
             df.groupby("political_belief")["TotalChoice"]
             .agg(["std", "mean"])
@@ -38,6 +47,18 @@ def analyze(df: pd.DataFrame, iteration: int, identity: int) -> None:
     # all identities
     else:
         output_file = "result/analysis_all_identities.xlsx"
+
+        # average yes by tweets
+        weights = df.groupby("tweet")["TotalChoice"].agg(["std", "mean"]).reset_index()
+        excel_write(weights, output_file, "weights")
+        # average yes by tweet class
+        weights_tweet_class = (
+            df.groupby("Tweet_classification")["TotalChoice"]
+            .agg(["std", "mean"])
+            .reset_index()
+        )
+        excel_write(weights_tweet_class, output_file, "weights_tweet_class")
+
         identities = [
             "education",
             "place",
@@ -46,13 +67,19 @@ def analyze(df: pd.DataFrame, iteration: int, identity: int) -> None:
             "personality",
         ]
         for ident in identities:
-            globals()[f"weights_{ident}"] = (
+            # average yes by tweet and identity
+            globals()[f"weights_{ident}_tweet"] = (
                 df.groupby(["tweet", ident])["TotalChoice"]
                 .agg(["std", "mean"])
                 .reset_index()
             )
-            excel_write(globals()[f"weights_{ident}"], output_file, f"weights_{ident}")
+            excel_write(
+                globals()[f"weights_{ident}_tweet"],
+                output_file,
+                f"weights_{ident}_tweet",
+            )
 
+            # average yes by tweet class and identity
             globals()[f"weights_{ident}_class"] = (
                 df.groupby([ident, "Tweet_classification"])["TotalChoice"]
                 .agg(["std", "mean"])
@@ -78,6 +105,7 @@ def analyze(df: pd.DataFrame, iteration: int, identity: int) -> None:
             + identities
             + [f"Total_{ident_abbr}" for ident_abbr in identities_abbrev]
         ]
+        # average identity mention by tweet class
         Mention_Class = (
             df.groupby("Tweet_classification")[
                 [f"Total_{ident_abbr}" for ident_abbr in identities_abbrev]
@@ -88,6 +116,7 @@ def analyze(df: pd.DataFrame, iteration: int, identity: int) -> None:
         excel_write(Mention_Class, output_file, "Mention_Class")
 
         for i in range(5):
+            # average identity mention by identity categories
             globals()[f"{identities_abbrev[i]}_Mention"] = (
                 df.groupby(identities[i])[f"Total_{identities_abbrev[i]}"]
                 .mean()
@@ -99,3 +128,20 @@ def analyze(df: pd.DataFrame, iteration: int, identity: int) -> None:
                 output_file,
                 f"{identities_abbrev[i]}_Mention",
             )
+
+
+if __name__ == "__main__":
+    df = pd.read_excel("result/coded_results.xlsx", sheet_name="No_Identity")
+    iteration = 30
+    identity = 0
+    analyze(df, iteration, identity)
+
+    df = pd.read_excel("result/coded_results.xlsx", sheet_name="Poli_Only")
+    iteration = 30
+    identity = 1
+    analyze(df, iteration, identity)
+
+    df = pd.read_excel("result/coded_results.xlsx", sheet_name="All_Identities")
+    iteration = 30
+    identity = 2
+    analyze(df, iteration, identity)
